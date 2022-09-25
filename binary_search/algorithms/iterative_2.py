3ddb9bc919570a16cc85c0d8b9528720686fd2a1
HabiburRahman1 <habib.usa2014@gmail.com>
2022-09-26 01:42:20 +0600

    Comments in the code

--- binary_search/algorithms/iterative_2.py
+++ binary_search/algorithms/iterative_2.py
@@ -1,18 +1,27 @@
+# This function implements the iterative binary search algorithm
 def binary_search_iterative(search_list, item):
+    # Set the initial values for the lower and upper bounds
     low = 0
     high = len(search_list) -1
 
+    # Loop until the higher bound - lower bound is greater than 1
     while high- low > 1:
+        # Calculate the midpoint
         mid = (low + high)//2
+        # if item is less than the midpoint, set the upper bound to the midpoint 
         if item < search_list[mid]:
             high = mid
+        # if item is greater than the midpoint, set the lower bound to the midpoint
         else:
             low = mid
         # break
+    # If the item is found in lower bound, return the index
     if search_list[low] == item:
         return "Item {} found in index {}".format(item, low)
+    # If the item is found in higher bound, return the index
     elif search_list[high] == item:
         return "Item {} found in index {}".format(item, high)
+    # If the item is not found, return None
     else:
         return "Item not found"
 
